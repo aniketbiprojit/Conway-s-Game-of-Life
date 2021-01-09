2ff24cd209fc7b5c33b8a2d536da5af84452555b
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2021-01-09 21:01:43 +0530

    feat: count neighbours and kill

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import App from './App.svelte'
 
 const num = 10
+
+const size = 40 //pixels
+
 const app = new App({
 	target: document.body,
 	props: {
@@ -12,12 +15,19 @@ const grid = Array.from(Array(10), () => new Array(10))
 
 class Cell {
 	block: HTMLElement
+	alive: boolean = false
 	constructor(block: HTMLElement) {
 		this.block = block
 		grid[parseInt(block.dataset.x)][parseInt(block.dataset.y)] = this
 		if (Math.random() < 0.5) {
 			block.classList.add('alive')
+			this.alive = true
 		}
+	}
+
+	kill() {
+		this.alive = false
+		this.block.classList.remove('alive')
 	}
 }
 
@@ -28,6 +38,26 @@ Array.from(cells).forEach((element) => {
 		new Cell(element)
 	}
 })
-console.log(grid)
+
+function update() {
+	for (let i = 0; i < num; i++) {
+		for (let j = 0; j < num; j++) {
+			// const cell = grid[i][j]
+			countNeighbours(i, j)
+		}
+	}
+}
+
+function countNeighbours(i: number, j: number) {
+	const count = 0
+	for (let x_off = -1; x_off < 1; x_off++) {
+		for (let y_off = -1; y_off < 1; y_off++) {
+			const x = (i + x_off + num) % num
+			const y = (j + y_off + num) % num
+		}
+	}
+}
+
+update()
 
 export default app
