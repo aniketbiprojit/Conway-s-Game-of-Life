99baba2e6572771cd5a3e7365d6f662001cee496
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2021-01-09 21:11:15 +0530

    fix: count neighbours

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ const app = new App({
 	},
 })
 
-const grid = Array.from(Array(10), () => new Array(10))
+const grid: Array<Array<Cell>> = Array.from(Array(10), () => new Array(10))
 
 class Cell {
 	block: HTMLElement
@@ -19,7 +19,7 @@ class Cell {
 	constructor(block: HTMLElement) {
 		this.block = block
 		grid[parseInt(block.dataset.x)][parseInt(block.dataset.y)] = this
-		if (Math.random() < 0.5) {
+		if (Math.random() < 0.1) {
 			block.classList.add('alive')
 			this.alive = true
 		}
@@ -28,6 +28,10 @@ class Cell {
 	kill() {
 		this.alive = false
 		this.block.classList.remove('alive')
+	}
+
+	setInner(text: string) {
+		this.block.children[0].innerHTML = text
 	}
 }
 
@@ -42,20 +46,30 @@ Array.from(cells).forEach((element) => {
 function update() {
 	for (let i = 0; i < num; i++) {
 		for (let j = 0; j < num; j++) {
-			// const cell = grid[i][j]
-			countNeighbours(i, j)
+			grid[i][j].setInner(countNeighbours(i, j).toString())
 		}
 	}
 }
 
 function countNeighbours(i: number, j: number) {
-	const count = 0
-	for (let x_off = -1; x_off < 1; x_off++) {
-		for (let y_off = -1; y_off < 1; y_off++) {
-			const x = (i + x_off + num) % num
+	let count = 0
+	for (let x_off = -1; x_off <= 1; x_off++) {
+		for (let y_off = -1; y_off <= 1; y_off++) {
+			const x = (i + x_off) % num
 			const y = (j + y_off + num) % num
+			if (x == -1) {
+				continue
+			}
+			console.log(x, y)
+			if (grid[x][y].alive) {
+				count++
+			}
 		}
 	}
+	if (grid[i][j].alive) {
+		count--
+	}
+	return count
 }
 
 update()
